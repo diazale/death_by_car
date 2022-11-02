03a7ae6b402cb28a05f7a02fbddc0d86be54e7da
diazale <alex.diazpapkovich@gmail.com>
2022-11-02 10:33:23 -0400

    data update

--- rss_checker.py
+++ rss_checker.py
@@ -57,7 +57,7 @@ google_french = True
 google_english = True
 
 # filter for the google search
-valid_dates = ['1 Nov']
+valid_dates = ['2 Nov']
 
 # Search for stories from selected RSS feeds
 if specific_news:
@@ -162,7 +162,8 @@ filtered_words = ["France", ".fr",".re","7sur7","Var-matin","Alsace","Martinique
                   "Le Berry","Nice Matin","Republican","Actu17","InsideNews","Yahoo","Marseille",
                   "Bordeaux","- Sud Ouest","Nice matin","L'Ardennais","Ardennais","DH Les Sports",
                   "La Presse de la Manche","Flandres","aufeminin","Est R","Plein Air","Chasse Passion",
-                  "AUNIS TV","L'essentiel","Gerardmer","Lorrain","Gedour","Hebdo","La Nouvelle"]
+                  "AUNIS TV","L'essentiel","Gerardmer","Lorrain","Gedour","Hebdo","La Nouvelle","Le Matin",
+                  "Corse-Matin"]
 
 filtered_urls = filtered_urls + used_urls
 
@@ -203,7 +204,7 @@ filtered_words = ["Boston.com","FOX","KTLA","KPRC","New York","Times of India","
                   "West Palm","York Daily Record","WHP Harrisburg","The News Leader","New Orleans",
                   "Suffolk Times","Deltaplex News","Modesto","KFVS","Arizona","Hudson","KSBY",
                   "Amarillo","WSMV","PennLive","Western Mass","Koam","USA TODAY","MLive.com",
-                  "Cincinnati","Cumbria Crack", "Surrey Live","Patriot Ledger"]
+                  "Cincinnati","Cumbria Crack", "Surrey Live","Patriot Ledger","bbc.com",".ie"]
 #filtered_urls = filtered_urls + used_urls
 
 rss = "https://news.google.ca/rss/search?q=" + "|".join(search_keys) + "&hl=en-CA&gl=CA&ceid=CA:en"
