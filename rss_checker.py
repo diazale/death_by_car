f28a126d3c974a1b2a3535db9cbd86b82c14c34d
diazale <alex.diazpapkovich@gmail.com>
2023-04-01 14:24:06 -0400

    data update

--- rss_checker.py
+++ rss_checker.py
@@ -59,7 +59,7 @@ google_french = True
 google_english = True
 
 # filter for the google search
-valid_dates = ['31 Mar']
+valid_dates = ['1 Apr']
 
 # Search for stories from selected RSS feeds
 if specific_news:
